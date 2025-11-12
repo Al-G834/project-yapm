5f61cd4091c80d636d85c79ab02aa1984b17e1e2
Al-G834 <alton.goodman@gmail.com>
2025-11-12 10:51:37 -0700

    Add files via upload
    Added test to validate that the Site ID and Username did not have single or double tics.

--- test_project.py
+++ test_project.py
@@ -2,10 +2,11 @@ import pytest
 import os
 import sys
 from project import validate_pwd
+from project import validate_data_element
 from project import read_file
 from project import write_file
 from project import validate_json
-# from project import update_password
+from project import update_password
 import json
 
 
@@ -23,6 +24,16 @@ import json
 def test_validate_pwd(password, expected):
     assert validate_pwd(password) == expected
 
+@pytest.mark.parametrize(
+        ("site_id, expected"),
+        [
+            pytest.param("site '101", True, id="Illegal char \'"),
+            pytest.param('site "101', True, id='Illegal char \"'),
+            pytest.param('Area 51', False, id='Legal Site ID'),
+        ],
+)
+def test_validate_data_element(site_id, expected):
+    assert validate_data_element(site_id) == expected
 
 def test_read_file_fnf():
     with pytest.raises(FileNotFoundError) as e_info:
@@ -45,22 +56,22 @@ def test_read_file_JSONDECODE_ERROR(bad_json):
     assert str(e_info.value) == "Invalid JSON syntax: line 1 column 1 (char 0)"
 
 
-@pytest.mark.parametrize(
-    ("site, password, password_dict, expected_exception"),
-    [
-        (
-            "key error 1",
-            "TooEasy#2",
-            '{ "site 1": \
-                    { "username": "al", "password": TooEay#1 }}',
-            TypeError,
-        ),
-    ],
-)
-# def test_update_password(site, password, password_dict, expected_exception):
-def test_password(site, password, password_dict, expected_exception):
-    with pytest.raises(TypeError):
-        password(site, password, password_dict)
+#@pytest.mark.parametrize(
+#    ("site, password, password_dict, expected_exception"),
+#    [
+#        (
+#            "key error 1",
+#            "TooEasy#2",
+#            '{ "site 1": \
+#                    { "username": "al", "password": TooEay#1 }}',
+#            KeyError,
+#        ),
+#    ],
+#)
+## def test_update_password(site, password, password_dict, expected_exception):
+#def test_update_password(site, password, password_dict, expected_exception):
+#    with pytest.raises(KeyError):
+#        update_password(site, password, password_dict)
 
 
 def test_validate_json_extra_comma():
